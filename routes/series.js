5e83a5be8a9c6a1dbe5954ffe3bb5d18a64cfb00
Alberto <albertodamianlopez@gmail.com>
2022-03-05 16:41:08 -0300

    Change comments in series.js

--- routes/series.js
+++ routes/series.js
@@ -3,17 +3,13 @@ const router = express.Router()
 const { check } = require('express-validator')
 const seriesControllers = require('../controllers/seriesControllers')
 
-
-//Get only movies
-//Endpoint: api/movies
-
+//Get only series
+//Endpoint: api/series
 
 router.get('/',
- //First checking authentication, then get only movies from this the database
               
     seriesControllers.getSeries
   
-    
 )
 module.exports = router;
 
