fd4ddc578f7c14cd9e3b429ed9f8d149ff56f830
Jicaffo <jicaffo@yahoo.com>
2022-03-04 22:40:32 -0300

    FAQ component optimized

--- client/src/Components/PreguntasFrecuentes/DropdownPreguntasFrecuentes/DropdownPreguntasFrecuentes.style.js
+++ client/src/Components/PreguntasFrecuentes/DropdownPreguntasFrecuentes/DropdownPreguntasFrecuentes.style.js
@@ -1,12 +1,10 @@
-
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    marginBottom: '4px'
-    
+    margin: '30px auto'
   },
   divider: {
     background: "black !important",
@@ -15,6 +13,8 @@ const useStyles = makeStyles((theme) => ({
   accordion: {
     background: theme.palette.background.card,
     color: theme.palette.background.input,
+    padding: ".5em 0",
+    marginBottom: "8px",
   },
   icon: {
     color: theme.palette.background.input,
