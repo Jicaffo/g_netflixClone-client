aa14b601e8aecd639d7e1e292bda94806ac12248
Alberto <albertodamianlopez@gmail.com>
2022-03-05 16:24:39 -0300

    Adding comments

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ app.use(express.json({ extender: true}))
 
 const PORT = process.env.PORT || 4000;
 
-
+// Autentificación (Login)
 app.use('/api/auth', require('./routes/auth'))
 
+// Creación de un nuevo usuario
 app.use('/api/users', require('./routes/users'))
 
+// Obtiene todas las series y peliculas
 app.use('/api/moviesAndSeries', require('./routes/moviesAndSeries'))
 
+// Obtiene todas las peliculas
 app.use('/api/movies', require('./routes/movies'))
 
+// Obtiene todas las series
 app.use('/api/series', require('./routes/series'))
 
+// Obtiene peliculas o series por un genero determinado
 app.use('/api/search/genre', require('./routes/searchByGenre'))
 
 app.listen(PORT, () =>{
