75b29ac00a98e95ce111b26a55c0cee6c195c5fb
Emanuel Constancio <emanuelconstancio.dev@gmail.com>
2022-03-05 16:16:31 -0300

    Endpoints reestructuration

--- index.js
+++ index.js
@@ -10,25 +10,30 @@ app.use(express.json({ extender: true}))
 
 const PORT = process.env.PORT || 4000;
 
-
+//Login
 app.use('/api/auth', require('./routes/auth'))
 
+//Register
 app.use('/api/users', require('./routes/users'))
 
+//Get all movies and series
 app.use('/api/moviesAndSeries', require('./routes/moviesAndSeries'))
 
-app.use('/api/movies', require('./routes/movies'))
+//Get By type
+app.use('/api/searchByType', require('./routes/searchByType'))
 
-app.use('/api/series', require('./routes/series'))
-
+//Get by Genre
 app.use('/api/search/genre', require('./routes/searchByGenre'))
+
+
+app.use('/api/recommendations', require('./routes/recommendations'))
 
 app.listen(PORT, () =>{
 
     console.log(`Server running on port ${PORT}`)
 })
 
-
+//i need to post the document and then search and find wich movies will be almost similar for the client
 
 
 
