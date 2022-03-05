435f121b312b196c349f9ab4c6ec12c8a5afe99d
Alberto <albertodamianlopez@gmail.com>
2022-03-05 16:38:15 -0300

    Delete spaces in movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -3,17 +3,13 @@ const router = express.Router()
 const { check } = require('express-validator')
 const moviesControllers = require('../controllers/moviesControllers')
 
-
 //Get only movies
 //Endpoint: api/movies
 
-
 router.get('/',
- //First checking authentication, then get only movies from this the database
-              
+
     moviesControllers.getMovies
   
-    
 )
 module.exports = router;
 
