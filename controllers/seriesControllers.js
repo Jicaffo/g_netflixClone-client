490db071a34d379ae659e461a8c0fbbbe004b2ad
Alberto <albertodamianlopez@gmail.com>
2022-03-05 16:35:40 -0300

    Detete spaces , change function comment SeriesCtrl

--- controllers/seriesControllers.js
+++ controllers/seriesControllers.js
@@ -1,22 +1,16 @@
 const MoviesAndSeries = require('../models/MoviesAndSeries')
-const successMessage = require('../shared/const/success.enums')
+// const successMessage = require('../shared/const/success.enums')
 
-//This function will get everything from moviesAndSeries collection.
+//This function will get only Series from moviesAndSeries collection.
 
 exports.getSeries = async(req,res) => {
 
-    
-
     try {
-        
         
         const series = await MoviesAndSeries.find({type: 'serie'})
   
-       
         res.json({series})
         //res.status(200).send(successMessage.sendSeries)
-
-
 
     } catch (error) {
         console.log(error)
