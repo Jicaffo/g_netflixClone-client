d404303847d28c53344a40c229ec34f77e04051c
AlvaroBarreira <alvarobarreiradev@gmail.com>
2022-03-08 01:23:16 -0300

    Route validation in header component fixed

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { useStyles } from './Header.styles';
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
 
     const classes = useStyles();
 
+    const location = useLocation();
+
     return (
         <div >
             <AppBar position="absolute" className={classes.root}>
                 <Toolbar className={classes.toolbar}>
-                    <svg className={classes.logo} />
-                    {window.location.pathname === "/" && <Link className={classes.iniciarSesion}>Iniciar sesion</Link>}                   
+                    <Link to={{ pathname: '/' }}>
+                        <svg className={classes.logo} />
+                     </Link>
+                    {location.pathname === "/" && <Link to={{ pathname: '/login' }} className={classes.iniciarSesion}>Iniciar sesion</Link>}                   
                 </Toolbar>
             </AppBar>
         </div>
