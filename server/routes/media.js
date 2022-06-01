f842dfc074b43a6e4e6128610fd07bffc6e9b10c
Jicaffo <jicaffo@yahoo.com>
2022-06-01 14:05:29 -0300

    Added authorization to media routes, getMedia controller now can filter by req.query, postMedia controller now can post multiple media through an array

--- server/routes/media.js
+++ server/routes/media.js
@@ -1,32 +1,37 @@
 import express from 'express';
 import { check } from 'express-validator';
 import mediaController from '../controllers/mediaController.js';
+import validateToken from '../middlewares/validateToken.js'
+import adminRequired from '../middlewares/adminRequired.js'
 
 const router = express.Router()
 
 // Endpoint: /api/media
 
-// Obtiene todos los recursos multimedia.
-router.get('/', mediaController.getAllMedia)
-
-// Obtiene un recurso por Id
-router.get('/:id', mediaController.getOneMediaById)
-
-// Interactuar con un recurso multimedia particular
-router.post('/', mediaController.postMedia)
-
-router.delete('/:id',
-    mediaController.deleteMedia
+// Obtiene todos los recursos multimedia (o una versión filtrada por req.query).
+router.get('/',
+    validateToken,
+    mediaController.getMedia
 )
 
-// Obtiene recursos filtrados // TODO: A implementar lógica como search params en /media: media?type=serie&genre=drama 
-// router.get('/byType=:type',
-//     mediaController.getMediaByType
-// );
+// Obtiene un recurso por Id
+router.get('/:id',
+    validateToken,
+    mediaController.getOneMediaById
+)
 
-// router.get('/byGenre=:genre',
-//     mediaController.getMediaByGenre
-// );
+// Interactuar con un recurso multimedia particular
+router.post('/',
+    validateToken,
+    adminRequired,
+    mediaController.postMedia
+)
+
+router.delete('/:id',
+    validateToken,
+    adminRequired,
+    mediaController.deleteMedia
+)
 
 // router.post('user/:userid/profile/:profileid/recommendations',
 // //First checking authentication, then get all recommedantions for this user
